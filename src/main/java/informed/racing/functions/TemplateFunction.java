3792e042830543afaa3a9494eacf2bb429c390df
Kevin O'Brien <kfcobrien@gmail.com>
2020-08-22 16:23:57 +0100

    removed xray context recovery: unsupported

--- src/main/java/informed/racing/functions/TemplateFunction.java
+++ src/main/java/informed/racing/functions/TemplateFunction.java
@@ -2,9 +2,6 @@ package informed.racing.functions;
 
 
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.entities.Segment;
-import com.amazonaws.xray.entities.TraceHeader;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
@@ -25,19 +22,8 @@ public class TemplateFunction implements Function<SQSEvent, String> {
     public String apply(SQSEvent sqs) {
         sqs.getRecords().forEach(record -> {
             log.info("message received: {}", record.getMessageId());
-            xrayContextRecovery(record.getAttributes().get("AWSTraceHeader"));
         });
         return "";
     }
 
-
-    private void xrayContextRecovery(String awsTraceHeader) {
-        if (awsTraceHeader != null) {
-            TraceHeader traceHeader = TraceHeader.fromString(awsTraceHeader);
-            Segment segment = AWSXRay.getCurrentSegment();
-            segment.setTraceId(traceHeader.getRootTraceId());
-            segment.setParentId(traceHeader.getParentId());
-            segment.setSampled(traceHeader.getSampled().equals(TraceHeader.SampleDecision.SAMPLED));
-        }
-    }
 }
