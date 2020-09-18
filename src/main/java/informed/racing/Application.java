0af8ec340895262da5a0129b52aa8ac0cb22d076
Kevin O'Brien <kfcobrien@gmail.com>
2020-09-18 14:45:24 +0100

    update

--- src/main/java/informed/racing/Application.java
+++ src/main/java/informed/racing/Application.java
@@ -4,11 +4,15 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import informed.racing.services.ExampleService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.function.Function;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "informed.racing")
+@EntityScan(basePackages = "informed.racing.common")
+@EnableJpaRepositories(basePackages = "informed.racing.common")
 public class Application {
 
     private final ExampleService exampleService;
@@ -16,7 +20,7 @@ public class Application {
     public Application(ExampleService exampleService) {
         this.exampleService = exampleService;
     }
-    
+
     @Bean("ExampleHandler")
     public Function<SQSEvent, String> parserJobHandler() {
         return exampleService::handle;
